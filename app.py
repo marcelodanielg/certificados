9d59d9eb93d34ef7209b82f62bc6df773bdf9261
marcelodanielg <marcelodanielg@gmail.com>
2026-03-12 13:22:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from reportlab.lib.utils import ImageReader
 
 # 1. CONFIGURACIÓN DE UBICACIÓN (Cambia estos números a tu gusto)
 # ----------------------------------------------------------------
-X_TEXTO = 150     # Centro de la hoja (Horizontal)
+X_TEXTO = 250     # Centro de la hoja (Horizontal)
 Y_TEXTO =  240     # Altura desde arriba (Vertical)
 TAMANO_LETRA = 20
 
@@ -78,6 +78,7 @@ if dni_input and df is not None:
         st.download_button("⬇️ DESCARGAR PDF", data=pdf, file_name=f"Certificado_{dni_input}.pdf")
     else:
         st.error("DNI no registrado.")
+
 
 
 
